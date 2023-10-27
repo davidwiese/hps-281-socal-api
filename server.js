4f376783d6e4e76fa8ec19cb00241f4e8b0e7052
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-10-27 12:58:48 -0700

    Test json response

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+const gorgonio = {
+  name: "San Gorgonio Mountain",
+  elevation: 11503,
+  range: "San Bernardino Mountains",
+  nickname: "Old Greyback",
+  complete: true,
+};
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api", (req, res) => {
+  res.json(gorgonio);
 });
 
 app.listen(PORT, () => {
