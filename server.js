75d2e038b0c34ccaabf2569770d2d4bcaa846f2f
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-10-27 14:03:45 -0700

    Add process.env.port for heroku

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.get("/api/:name", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is running on port ${PORT}! You better get moving!`);
 });
