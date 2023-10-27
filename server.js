b465b7e2dffb49722be81794d041b69fc6921327
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-10-27 13:17:38 -0700

    Rename api object, add some peaks

--- server.js
+++ server.js
@@ -2,12 +2,55 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-const gorgonio = {
-  name: "San Gorgonio Mountain",
-  elevation: 11503,
-  range: "San Bernardino Mountains",
-  nickname: "Old Greyback",
-  complete: true,
+const hps281socal = {
+  gorgonio: {
+    name: "San Gorgonio Mountain",
+    elevation: 11503,
+    range: "San Bernardino Mountains",
+    section: "San Gorgonio Area",
+    nickname: "Old Greyback",
+    complete: true,
+  },
+  jepson: {
+    name: "Jepson Peak",
+    elevation: 11205,
+    range: "San Bernardino Mountains",
+    section: "San Gorgonio Area",
+    nickname: "",
+    complete: true,
+  },
+  bighorn: {
+    name: "Bighorn Mountain",
+    elevation: 10997,
+    range: "San Bernardino Mountains",
+    section: "San Gorgonio Area",
+    nickname: "",
+    complete: false,
+  },
+  dragonshead: {
+    name: "Dragons Head",
+    elevation: 10866,
+    range: "San Bernardino Mountains",
+    section: "San Gorgonio Area",
+    nickname: "",
+    complete: false,
+  },
+  anderson: {
+    name: "Anderson Peak",
+    elevation: 10840,
+    range: "San Bernardino Mountains",
+    section: "San Gorgonio Area",
+    nickname: "",
+    complete: true,
+  },
+  jacinto: {
+    name: "San Jacinto Peak",
+    elevation: 10839,
+    range: "San Jacinto Mountains",
+    section: "San Jacinto Area",
+    nickname: "",
+    complete: true,
+  },
 };
 
 app.get("/", (req, res) => {
@@ -15,7 +58,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api", (req, res) => {
-  res.json(gorgonio);
+  res.json(hps281socal);
 });
 
 app.listen(PORT, () => {
